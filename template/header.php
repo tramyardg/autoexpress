92ca4b0dc095672ad2c3b4e0299276302655e701
Raymart De Guzman <tramyardg@gmail.com>
2018-06-01 00:20:47 -0400

    #14 link redirection for dev. and production

--- template/header.php
+++ template/header.php
@@ -3,7 +3,10 @@
     <div class="clear-both"></div>
     <div class="menu-header">
         <div id="menu-header-left-section">
+            <!-- for development
             <span><a href="index.php"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Used Vehicles</a></span>
+             -->
+            <span><a href="http://autoexpress.co.nf"><i class="fa fa-home" aria-hidden="true"></i>&nbsp;Used Vehicles</a></span>
         </div>
         <div class="clear-both"></div>
     </div>
