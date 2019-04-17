e126c260da0ae6efa0d98c1b41202e3b0029ff6e
Raymart De Guzman <tramyardg@gmail.com>
2019-04-17 17:30:53 -0400

    meta tags updated

--- admin/logout.php
+++ admin/logout.php
@@ -1,14 +1,13 @@
-<?php ob_start(); session_start(); ?>
+<?php ob_start();
+session_start(); ?>
 
-<html>
+<html lang="en">
 <body>
 <p>Signing out... Please wait.</p>
 <?php
 if (isset($_SESSION['authenticated']) && $_SESSION['authenticated'] == 1) {
     session_destroy();
     header('refresh: 1; url=sign-in.php');
-} else {
-
 }
 ?>
 </body>
--- index.php
+++ index.php
@@ -19,10 +19,21 @@ $rowCarField = $v->getCarsLimitByRecPerPage($p->getStartingRow(), $p->getRecords
 $carObjSearchResult = $s->getSearchInputResult('search-car');
 //print_r($carObjSearchResult);
 ?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>AutoExpress.com</title>
+    <!-- Primary Meta Tags -->
+    <title>AutoExpress.co.nf - Raymart De Guzman</title>
+    <meta name="title" content="AutoExpress.co.nf - Raymart De Guzman">
+    <meta name="description" content="Autoexpress.co.nf is a car dealership app built for both car dealer and car buyer. A car dealer manages the car being viewed on the website by adding, updating, deleting and uploading photos of a car. On the other hand, a car buyer can search for the vehicle he or she desired on the website. If the buyer finds the desired vehicle he or she can contact the seller to get more information of the vehicle. A car buyer can also calculate their monthly or bi-weekly payment.">
+    <meta name="keywords" content="Raymart De Guzman, tramyardg.co.nf, tramyardg, PHP, car dealership, software engineer, car dealer, car buyer, AutoExpress.co.nf">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://autoexpress.co.nf"/>
+    <meta property="og:title" content="AutoExpress.co.nf - Raymart De Guzman">
+    <meta property="og:description" content="Autoexpress.co.nf is a car dealership app built for both car dealer and car buyer. A car dealer manages the car being viewed on the website by adding, updating, deleting and uploading photos of a car. On the other hand, a car buyer can search for the vehicle he or she desired on the website. If the buyer finds the desired vehicle he or she can contact the seller to get more information of the vehicle. A car buyer can also calculate their monthly or bi-weekly payment.">
+    <meta property="og:image" content="https://raw.githubusercontent.com/tramyardg/autoexpress/blob/master/image/home_page.PNG"/>
 
     <link rel="apple-touch-icon" sizes="180x180" href="image/favicon_package_v0.16/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="image/favicon_package_v0.16/favicon-32x32.png">
