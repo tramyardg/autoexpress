48f90294560ff192c1a36260178439ba1291c000
Raymart De Guzman <tramyardg@gmail.com>
2019-08-06 21:49:01 -0400

    social plugin: facebook share button in progress

---
+++ js/FbShare.js
@@ -0,0 +1,8 @@
+// Reference: https://developers.facebook.com/docs/plugins/share-button/#settings
+// dynamically create facebook share button to share a car
+
+class FbShare {
+  constructor(url, layout, buttonSize) {
+
+  }
+}
